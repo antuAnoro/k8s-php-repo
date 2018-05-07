94cf3eb355255e89ed2fbc3ca1ececb2b94ac5e4
antuAnoro <antonio_manteca@hotmail.com>
2018-05-07 21:47:11 +0200

    Añadimos hostname en titulo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 
 <html>
 <head>	
-	<title>Homepage</title>
+	<title>Homepage en <?php echo gethostname(); ?></title>
 </head>
 
 <body>
